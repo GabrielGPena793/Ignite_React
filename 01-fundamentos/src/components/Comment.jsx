051031df89b747c7b132e8a51162e8961de2c1bb
gabrielgpena793 <kbeca.gabriel793@gmail.com>
2022-06-26 23:02:11 -0300

    commment struct html

---
+++ 01-fundamentos/src/components/Comment.jsx
@@ -0,0 +1,36 @@
+import { ThumbsUp, Trash } from "phosphor-react"
+import styles from "./Comment.module.css"
+
+export function Comment(){
+  return (
+    <div className={styles.comment}>
+      <img src="https://github.com/gabrielgpena793.png" alt="" />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Gabriel Gomes</strong>
+              <time title="22 de Junho ás 23:00" dateTime="2022-06-22 23:00:00">
+                Cerca de 1h atrás
+              </time>
+            </div>
+
+            <button title="Deletar comentário">
+              <Trash size={20} />
+            </button>
+          </header>
+
+          <p>Muito bom Dev, parabéns!!</p>
+        </div>
+
+        <footer>
+          <button>
+            <ThumbsUp />
+            Aplaudir <span>28</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  )
+}
