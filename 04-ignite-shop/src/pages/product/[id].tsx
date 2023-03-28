d1396122545845955e9ebd90d6fb4aaca544a07c
gabrielgpena793 <kbeca.gabriel793@gmail.com>
2023-03-28 02:41:48 -0300

    Produto & Checkout - Fallback do SSG

--- 04-ignite-shop/src/pages/product/[id].tsx
+++ 04-ignite-shop/src/pages/product/[id].tsx
@@ -1,6 +1,6 @@
 import { stripe } from '@/src/lib/stripe'
 import { ImageContainer, ProductContainer, ProductDetails } from '@/src/styles/pages/product'
-import { GetStaticProps } from 'next'
+import { GetStaticPaths, GetStaticProps } from 'next'
 import Image from 'next/image'
 import { useRouter } from 'next/router'
 import Stripe from 'stripe'
@@ -16,6 +16,12 @@ interface ProductProps {
 }
 
 export default function Product({ product }: ProductProps) {
+
+  const { isFallback } = useRouter()
+
+  if(isFallback) {
+    return <p>Loading...</p>
+  }
 
   return (
     <ProductContainer>
@@ -34,6 +40,30 @@ export default function Product({ product }: ProductProps) {
       </ProductDetails>
     </ProductContainer>
   )
+}
+
+/* 
+  É necessário usar o método getStaticPaths quando queremos criar versões státicas de paginas
+  que são variáveis ex: [id].tsx, que vão criar varias paginas uma para cada produto.
+  No método podemos passar alguns paths já na execução, tendo assim algumas paginas logo de 
+  cara prontas no cache, e deixar o restante para que o método crie quando for preciso
+  ex:  paths: [{id : 'idDaPage}]
+  Também podemos deixar o array vazio e deixar que ele crie tudo sob demanda.
+
+  Usamos também o fallback para termos acesso ao estado da page e saber se já foram concluídos
+  a request dos dados, para que a pagina não tenha um carregamento antes desses dados sejam concluídos
+  O fallback tem 3 valores
+  false -> ele não espera e já trás a pagina antes da requisição podendo ocorrer um erro, caso a pagina não tenha sido carregada antes
+  true -> ele me da acesso ao estado do pagina, que através do useRouter(), conseguimos pegar o isFallback, tendo assim o controle
+          da pagina para colocarmos um load e previnir erros
+  blocking -> Ele por si só já faz o bloqueio e só vai mostrar a pagina quando tiver tudo ok, porém pode demorar um pouco
+*/
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  return {
+    paths: [],
+    fallback: true,
+  }
 }
 
 
