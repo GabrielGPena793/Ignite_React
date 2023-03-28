724a02c806322de204e0220cb5fd1e740dd2aada
gabrielgpena793 <kbeca.gabriel793@gmail.com>
2023-03-27 23:12:49 -0300

    config image accept others sites

--- 04-ignite-shop/src/pages/index.tsx
+++ 04-ignite-shop/src/pages/index.tsx
@@ -8,57 +8,71 @@ import camiseta1 from '../assets/1.png'
 import camiseta2 from '../assets/2.png'
 import camiseta3 from '../assets/3.png'
 
+import { stripe } from '../lib/stripe'
+import { GetServerSideProps } from 'next'
+
 import 'keen-slider/keen-slider.min.css';
+import Stripe from 'stripe'
 
-export default function Home() {
+interface HomeProps {
+  products:
+  {
+    id: string;
+    name: string;
+    imageUrl: string;
+    price: number;
+  }[]
+}
 
- const [sliderRef] = useKeenSlider({
+export default function Home({ products }: HomeProps) {
+
+  const [sliderRef] = useKeenSlider({
     slides: {
       perView: 2,
       spacing: 48,
     }
- })
+  })
 
   return (
     <HomeContainer ref={sliderRef} className='keen-slider'>
-      <Product className='keen-slider__slide'>
-        <Image src={camiseta1} width={520} height={480} alt='' />
+      {products.map(product => (
+        <Product key={product.id} className='keen-slider__slide'>
+          <Image src={product.imageUrl} width={520} height={480} alt='' />
 
-        <footer>
-          <strong>Camiseta X</strong>
-          <span>R$ 79,90</span>
-        </footer>
-      </Product>
-
-      <Product className='keen-slider__slide'>
-        <Image src={camiseta2} width={520} height={480} alt='' />
-
-         <footer>
-          <strong>Camiseta X</strong>
-          <span>R$ 79,90</span>
-        </footer>
-      </Product>
-
-      <Product className='keen-slider__slide'>
-        <Image src={camiseta3} width={520} height={480} alt='' />
-
-         <footer>
-          <strong>Camiseta X</strong>
-          <span>R$ 79,90</span>
-        </footer>
-      </Product>
-
-      <Product className='keen-slider__slide'>
-        <Image src={camiseta3} width={520} height={480} alt='' />
-
-         <footer>
-          <strong>Camiseta X</strong>
-          <span>R$ 79,90</span>
-        </footer>
-      </Product>
-
-
-
+          <footer>
+            <strong>{product.name}</strong>
+            <span>{product.price}</span>
+          </footer>
+        </Product>
+      ))}
     </HomeContainer>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  // expand: serve para adicionar o relacionamento que o produto tem com o preço no stripe
+  // isso serve para qualquer relação que tem no stripe, como é uma lista o data sempre vem antes
+  // se não fosse, caso fosse apenas um produto acessaríamos diretamente a chave do relacionamento
+  const response = await stripe.products.list({
+    expand: ['data.default_price']
+  });
+
+  const products = response.data.map(product => {
+
+    const priceModel = product.default_price as Stripe.Price
+    const price = priceModel.unit_amount ? priceModel.unit_amount / 100 : 0
+
+    return {
+      id: product.id,
+      name: product.name,
+      imageUrl: product.images[0],
+      price,
+    }
+  })
+
+  return {
+    props: {
+      products
+    }
+  }
 }
